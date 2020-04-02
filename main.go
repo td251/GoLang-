a4e6f1bf326c7e661d50d90e540e256e58a57136
td251 <tylerdaniel@btinternet.com>
2020-04-02 22:45:58 +0100

    Feat: On LocalHost

---
+++ main.go
@@ -0,0 +1,18 @@
+package main 
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request){
+    fmt.Fprintf(w, "Tylers Homepage")
+    fmt.Println("Endpoint Hit: homePage")
+}
+func handleRequests() {
+    http.HandleFunc("/", homePage)
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
+func main() {
+    handleRequests()
+}
