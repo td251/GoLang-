55958ae5a971871d7438092ca6299015feffd5cd
td251 <tylerdaniel@btinternet.com>
2020-04-02 23:02:14 +0100

    Feat: adding article page

--- main.go
+++ main.go
@@ -1,18 +1,41 @@
-package main 
+package main
+
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Tylers Homepage")
-    fmt.Println("Endpoint Hit: homePage")
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Tylers Homepage")
+	fmt.Println("Endpoint Hit: homePage")
 }
 func handleRequests() {
-    http.HandleFunc("/", homePage)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/", homePage)
+	//adding the article route\
+	http.HandleFunc("/articles", getAllArticles)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 func main() {
-    handleRequests()
+	handleRequests()
+}
+
+//struct structure of data fields
+//with declared data
+type Article struct {
+	Title       string `json:"title"`
+	Description string `json:"desc"`
+	Content     string `json:"content"`
+}
+
+//declaring an articles array
+//simulate a db
+var Articles []Article
+
+//CRUD
+//retiving all the articles
+func getAllArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Hit! Return articles")
+	json.NewEncoder(w).Encode(Articles)
 }
