479401bf590269aa150ce37977fadd647b3899ce
td251 <tylerdaniel@btinternet.com>
2020-04-02 23:18:57 +0100

    Feat: Getting dummy data

--- main.go
+++ main.go
@@ -18,12 +18,20 @@ func handleRequests() {
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 func main() {
+	Articles = []Article{
+        Article{Id: "1", Title: "YO ", Description: "Hmmmmmm", Content: "Content"},
+		Article{Id: "2", 
+		Title: "Hello 2", 
+		Description: "Description", 
+		Content: "Article Content"},
+    }
 	handleRequests()
 }
 
 //struct structure of data fields
 //with declared data
 type Article struct {
+	Id      	string `json:"Id"`
 	Title       string `json:"title"`
 	Description string `json:"desc"`
 	Content     string `json:"content"`
@@ -31,11 +39,24 @@ type Article struct {
 
 //declaring an articles array
 //simulate a db
-var Articles []Article
+var Articles [
+]Article
 
 //CRUD
 //retiving all the articles
 func getAllArticles(w http.ResponseWriter, r *http.Request) {
+	// articles := Articles{
+	// 	Article{
+	// 	Title:" Blah Blah Test ", 
+	// 	desc: "Description", 
+	// 	content:"Content"}
+	// },
+	// {
+	// 	Article{
+	// 	Title:" Blah Blah Test 101", 
+	// 	desc: "Description 101", 
+	// 	content:"Content 101"}
+	// }
 	fmt.Println("Hit! Return articles")
 	json.NewEncoder(w).Encode(Articles)
 }
